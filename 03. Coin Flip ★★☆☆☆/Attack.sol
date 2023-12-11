3ae36c66faa1764676bea41e526de31f021707fc
TimOu <99255480+timou0911@users.noreply.github.com>
2023-12-11 20:51:30 +0800

    Update Attack.sol

--- 03. Coin Flip ★★☆☆☆/Attack.sol
+++ 03. Coin Flip ★★☆☆☆/Attack.sol
@@ -9,7 +9,7 @@ contract Attack {
     uint256 public consecutiveWins;
     uint256 lastHash;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    address target;
+    address private immutable target;
 
     constructor(address targetAddr) {
         target = targetAddr;
