54722007aea7bbc790f8cf2924ed98cd37a75463
TimOu <99255480+timou0911@users.noreply.github.com>
2023-12-11 20:53:01 +0800

    Update Attack.sol

--- 07. Force  ★★★☆☆/Attack.sol
+++ 07. Force  ★★★☆☆/Attack.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract Attack {
 
-    address payable targetAddr;
+    address payable private immutable targetAddr;
 
     constructor(address addr) payable { // send ethers to the contract when deploying it
         targetAddr = payable(addr);
