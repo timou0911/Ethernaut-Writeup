b6b501ac2db8ca0eeef1931fc62a75072902bbb3
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-22 18:05:26 +0800

    Update FakeToken.sol

--- 23. Dex Two ★★☆☆☆/FakeToken.sol
+++ 23. Dex Two ★★☆☆☆/FakeToken.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+// 0x0Ec24374999dad931E4477aFF98bc5f563bE5005
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FakeToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("FakeToken", "FAKE") {
+        _mint(msg.sender, initialSupply); // provide at least 400 tokens
+    }
+}
